2994ead0eef9e89d9472be8a2df9c44290656189
Luigi Micco <luigi.micco@gmail.com>
2025-01-31 20:02:12 +0100

    add API routes

--- bin/dart_server_http.dart
+++ bin/dart_server_http.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'dart:convert';
 
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
@@ -6,6 +7,7 @@ import 'package:shelf_router/shelf_router.dart';
 
 final router = Router()
   ..get('/', _rootHandler)
+  ..mount('/api/v1.0/', Api().router.call)
   ..all('/<ignored|.*>', (Request request) {
     return Response.notFound('Page not found');
   });
@@ -24,4 +26,29 @@ void main(List<String> arguments) async {
 
   final server = await serve(handler, ip, port);
   print('Server listening ${server.address.host}:${server.port}');
+}
+
+class Api {
+  List<int> items = [1, 2, 3, 4, 5];
+
+  Future<Response> _items(Request request) async {
+    return Response.ok(jsonEncode(items),
+        headers: {'Content-Type': 'application/json'});
+  }
+
+  Router get router {
+    final router = Router();
+
+    router.get('/items', _items);
+    router.post('/items', (Request request) async {
+      final body = await request.readAsString();
+      final item = int.parse(body);
+      items.add(item);
+      return Response.ok(jsonEncode(items),
+          headers: {'Content-Type': 'application/json'});
+    });
+    router.all('/<ignored|.*>', (Request request) => Response.notFound('null'));
+
+    return router;
+  }
 }
