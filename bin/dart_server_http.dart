c4797eea651bfbac0dbc44f129d22b6fe8604fda
Luigi Micco <luigi.micco@gmail.com>
2025-01-31 14:39:46 +0100

    advanced elements

--- bin/dart_server_http.dart
+++ bin/dart_server_http.dart
@@ -1,18 +1,26 @@
+import 'dart:io';
+
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
 import 'package:shelf_router/shelf_router.dart';
 
-final router = Router()..get('/', _rootHandler);
+final router = Router()
+  ..get('/', _rootHandler)
+  ..all('/<ignored|.*>', (Request request) {
+    return Response.notFound('Page not found');
+  });
 
 Response _rootHandler(Request req) {
-  return Response.ok('Hello, World!\n');
+  return Response.ok('<h1>Hello, World!</h1>\n',
+      headers: {'Content-Type': 'text/html'});
 }
 
 void main(List<String> arguments) async {
-  final ip = "127.0.0.1";
-  final port = 8080;
+  final ip = InternetAddress.anyIPv4;
+  final port = int.parse(Platform.environment['PORT'] ?? '8080');
 
-  final handler = Pipeline().addHandler(router.call);
+  final handler =
+      Pipeline().addMiddleware(logRequests()).addHandler(router.call);
 
   final server = await serve(handler, ip, port);
   print('Server listening ${server.address.host}:${server.port}');
